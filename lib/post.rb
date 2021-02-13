28d0b7d128cb7d6dbcad8da5111c58f130284841
Christopher Garcia <christopher.garcia08@gmail.com>
2021-02-13 04:17:12 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,22 +1,15 @@
 class Post 
   attr_accessor :title, :author 
   
-  @@all = [] 
-  
   def initialize(title)
     @title = title 
-    @all < self
-  end
-  
-  def self.all
-    @@all
   end
   
   def author_name
-    if self.author
-      self.author.name 
+    if @author == nil
+      return nil 
     else
-      NIL
+      @author.name
     end
   end
 

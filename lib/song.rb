47b3c790cb806aaff6ca6d3bbbea74d2d63e49c8
Christopher Garcia <christopher.garcia08@gmail.com>
2021-02-13 04:18:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,15 @@
 class Song 
   attr_accessor :name, :artist 
-  
-  @@all = [] 
-  
+
   def initialize(name)
     @name = name 
-    @all < self
-  end
-  
-  def self.all
-    @@all
   end
   
   def artist_name
-    if self.artist
-      self.artist.name 
+    if @artist == nil
+      return nil
     else
-      NIL
+      @artist.name
     end
   end
   

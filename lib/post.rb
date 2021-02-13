7ec88e19ccf2b1f9da1673539b7e354ad5f04ce2
Christopher Garcia <christopher.garcia08@gmail.com>
2021-02-13 04:03:17 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -7,5 +7,17 @@ class Post
     @title = title 
     @all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if self.author
+      self.author.name 
+    else
+      NIL
+    end
+  end
 
 end

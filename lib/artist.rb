e4906e934e56ea380fc911513bc3cb9d80f3d821
Christopher Garcia <christopher.garcia08@gmail.com>
2021-02-13 03:56:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,4 +8,23 @@ class Artist
     @songs = []
   end
   
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song(song)
+    @@song_count += 1 
+    @songs << song 
+    song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    @@song_count += 1 
+    @songs << song 
+    song.artist = self 
+  end
+  
+  def self.song_count
+    @@songcount 
+  end
 end

a938263d2beb26d9d3a07e8ce10ec305f861ba04
Christopher Garcia <christopher.garcia08@gmail.com>
2021-02-13 03:57:49 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,5 +9,22 @@ class Author
   end
   
   def posts
-    Post.all.select 
+    Post.all.select {|post| post.author = self}
+  end
+  
+  def add_song(song)
+    @@song_count += 1 
+    @songs << song 
+    song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    @@song_count += 1 
+    @songs << song 
+    song.artist = self 
+  end
+  
+  def self.song_count
+    @@songcount 
+  end
 end

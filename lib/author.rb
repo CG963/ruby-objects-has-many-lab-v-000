8c2757b8fd11c7e2aa718fc4d370f3641d85e784
Christopher Garcia <christopher.garcia08@gmail.com>
2021-02-13 04:22:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,10 @@
 class Author 
-  attr_accessor :name, :posts 
+  attr_accessor :name
   
   @@post_count = 0 
   
   def initialize(name)
     @name = name 
-    @posts = []
   end
   
   def add_post(post)
